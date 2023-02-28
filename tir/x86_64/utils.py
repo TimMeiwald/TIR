a535e8b51d2956f2dac993ab3adec1b83d835019
Tim Meiwald <tim.meiwald@gmail.com>
2023-02-28 19:31:26 +0000

    Moved modrm out of main x86-64 file since it's used everywhere

---
+++ tir/x86_64/utils.py
@@ -0,0 +1,9 @@
+def modrm(mod: int, r: int, m: int):
+    """Intel Volume 2A 2-6
+    
+    r changes row
+    m changes column
+    Not sure which way it's meant to be as long as you're consistent works."""
+    mod = mod*2**6
+    m = m*2**3
+    return mod+r+m
--- tir/x86_64/x86_64.py
+++ tir/x86_64/x86_64.py
@@ -1,19 +1,9 @@
 from elfgenerator.Binary import Binary
 
 from enum import IntEnum
-
+from tir.x86_64.utils import modrm
 def syscall():
     return Binary(0x050F, 2, 2)
-
-def modrm(mod: int, r: int, m: int):
-    """Intel Volume 2A 2-6
-    
-    r changes row
-    m changes column
-    Not sure which way it's meant to be as long as you're consistent works."""
-    mod = mod*2**6
-    m = m*2**3
-    return mod+r+m
 
 def signed_multiply_register_one_with_register_two(reg1: int, reg2: int):
     opcode = Binary(0x0f, 1, 1)
